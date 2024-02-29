4d79f16252301ffc9a91ce2e664db44ccbaf7413
desync <blurskye@proton.me>
2024-02-29 09:23:55 +0600

    debug commit

--- lua/lazygit.lua
+++ lua/lazygit.lua
@@ -56,10 +56,24 @@ end
 --     vim.cmd('startinsert')
 -- end
 function M.toggle_git()
-    if vim.api.nvim_buf_get_name(0) == " LAZYGIT" then
-        vim.api.nvim_win_hide(M.git_win)
-        M.git_win = nil
-    elseif M.git_buf == nil or not vim.api.nvim_buf_is_valid(M.git_buf) or vim.fn.jobwait({ vim.api.nvim_buf_get_option(M.git_buf, 'channel') }, 0)[1] == -1 then
+    if M.git_buf and vim.api.nvim_buf_is_valid(M.git_buf) then
+        -- If the git window is open, hide it
+        if M.git_win and vim.api.nvim_win_is_valid(M.git_win) then
+            vim.api.nvim_win_hide(M.git_win)
+            M.git_win = nil
+        else
+            -- If the git window is not open, open it
+            M.git_win = vim.api.nvim_open_win(M.git_buf, true, {
+                relative = "editor",
+                width = vim.o.columns,
+                height = vim.o.lines - 1, -- Subtract 1 to leave space for the status line
+                col = 0,
+                row = 0,
+            })
+            vim.cmd('startinsert')
+        end
+    else
+        -- If the git buffer does not exist or is not valid, create it
         M.git_buf = vim.api.nvim_create_buf(false, true)
         M.git_win = vim.api.nvim_open_win(M.git_buf, true, {
             relative = "editor",
@@ -72,27 +86,11 @@ function M.toggle_git()
         vim.api.nvim_buf_set_option(M.git_buf, 'bufhidden', 'hide')
         vim.fn.termopen("lazygit")
 
-        -- Set buffer name to "LAZYGIT"
-        vim.api.nvim_buf_set_name(M.git_buf, " LAZYGIT")
-
         -- Hide line numbers in the git window
         vim.api.nvim_win_set_option(M.git_win, 'number', false)
         vim.api.nvim_win_set_option(M.git_win, 'relativenumber', false)
-    else
-        if M.git_win ~= nil and vim.api.nvim_win_is_valid(M.git_win) then
-            vim.api.nvim_win_hide(M.git_win)
-            M.git_win = nil
-        else
-            M.git_win = vim.api.nvim_open_win(M.git_buf, true, {
-                relative = "editor",
-                width = vim.o.columns,
-                height = vim.o.lines - 1, -- Subtract 1 to leave space for the status line
-                col = 0,
-                row = 0,
-            })
-        end
+        vim.cmd('startinsert')
     end
-    vim.cmd('startinsert')
 end
 
 return M
