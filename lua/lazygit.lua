59a7a1fb01928f230df66275965fb43ec7c95e30
desync <blurskye@proton.me>
2024-02-29 12:59:53 +0600

    debug commit

--- lua/lazygit.lua
+++ lua/lazygit.lua
@@ -31,7 +31,8 @@ function M.resize_git_win()
 
             -- Set the current window to the git window
             vim.api.nvim_set_current_win(M.git_win)
-            vim.cmd('normal! gg^') -- Go to the first non-blank character of the first line
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc><Esc>', true, true, true), 't', true)
+            vim.cmd('normal! gg^i') -- Go to the first non-blank character of the first line
 
 
             -- Ensure we're in normal mode, then reorient the git window
